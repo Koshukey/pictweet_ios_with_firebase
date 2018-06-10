cae5cd2562148c5d038a0e3799e06368b68bcaa9
Kosukey <koshuke0217@icloud.com>
2018-06-10 18:35:46 +0900

    make form better design by native base

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -3,34 +3,34 @@ import { View, Text, Picker } from 'react-native';
 import { CardSection, Input } from "./common";
 import { connect } from 'react-redux';
 import { employeeUpdate } from "../actions";
+import { Container, Header, Left, Body, Title, Right, Content, Textarea, Form, Button} from "native-base";
 
 class EmployeeForm extends Component {
     render() {
         return (
-            <View>
-
-                <CardSection>
-                    <Input
-                        label="Image"
-                        placeholder="tom"
+            <View style={{height:300}}>
+            <Container>
+                <Content padder>
+                    <Form>
+                        <Textarea
+                        rowSpan={3}
+                        bordered
+                        placeholder="ImageUrl"
                         value={this.props.imageUrl}
                         onChangeText={text=> this.props.employeeUpdate({ prop: 'imageUrl', value: text})}
-                    />
+                        />
+                        <Textarea
+                            rowSpan={5}
+                            bordered
+                            placeholder="Text"
+                            value={this.props.text}
+                            onChangeText={text=> this.props.employeeUpdate({ prop:'text', value: text})}
+                        />
+                        </Form>
 
-                </CardSection>
+                </Content>
 
-                <CardSection>
-                    <Input
-                        label="Text"
-                        placeholder="111111"
-                        value={this.props.text}
-                        onChangeText={text=> this.props.employeeUpdate({ prop:'text', value: text})}
-                    />
-                </CardSection>
-
-
-
-
+            </Container>
             </View>
         )
 
