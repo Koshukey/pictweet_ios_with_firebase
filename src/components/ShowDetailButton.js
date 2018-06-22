8af440c4e46bc8c2cd2b5ebc70d93bdbc949d54b
Kosukey <koshuke0217@icloud.com>
2018-06-22 17:56:36 +0900

    サンプル画像の追加

---
+++ src/components/ShowDetailButton.js
@@ -0,0 +1,45 @@
+// import React, { Component } from 'react';
+// import { Text, TouchableWithoutFeedback, View, Image, ImageBackground  } from 'react-native';
+// import { Actions } from 'react-native-router-flux';
+// import AwesomeButtonRick from 'react-native-really-awesome-button';
+// import ListItem from "./ListItem";
+//
+//
+// class ShowDetailButton extends Component {
+//
+//     onDetailButtonPress() {
+//         Actions.showDetail();
+//
+//     }
+//
+//     render() {
+//
+//         const { detailButtonStyle } = styles;
+//
+//
+//         return (
+//                 <AwesomeButtonRick
+//                     size="small"
+//                     type="primary"
+//                     width={380}
+//                     textColor="#FFFFFF"
+//                     height={40}
+//                     backgroundColor="#1E90FF"
+//                     onPress={this.onDetailButtonPress.bind(this)}
+//                 >show detail</AwesomeButtonRick>
+//         );
+//     }
+//
+//
+// }
+//
+//
+//
+// const styles = {
+//
+//     detailButtonStyle: {
+//         height: 5
+//     }
+// };
+//
+// export default ShowDetailButton;
